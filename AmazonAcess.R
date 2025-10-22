bddd6dabc0683dfba745ea9744d6ae380fa2721a
Sam Kirkham <21skirkham@gmail.com>
2025-10-21 21:55:16 -0600

    Added K-Nearest Neighbors Model

--- AmazonAcess.R
+++ AmazonAcess.R
@@ -16,24 +16,24 @@ trainData <- vroom("train.csv") %>%
 
 ############################################################################
 
-EDA
-trainData <- trainData %>%
-  mutate(ACTION = as.factor(ACTION)) %>%
-  mutate(RESOURCE = as.factor(RESOURCE)) %>%
-  mutate(MGR_ID = as.factor(MGR_ID)) %>%
-  mutate(ROLE_ROLLUP_1 = as.factor(ROLE_ROLLUP_1)) %>%
-  mutate(ROLE_ROLLUP_2 = as.factor(ROLE_ROLLUP_2)) %>%
-  mutate(ROLE_DEPTNAME = as.factor(ROLE_DEPTNAME)) %>%
-  mutate(ROLE_TITLE = as.factor(ROLE_TITLE)) %>%
-  mutate(ROLE_FAMILY_DESC = as.factor(ROLE_FAMILY_DESC)) %>%
-  mutate(ROLE_FAMILY = as.factor(ROLE_FAMILY)) %>%
-  mutate(ROLE_CODE = as.factor(ROLE_CODE))
-
-# step_mutate_at(all_numeric_predictors(), fn = factor)
-
-
-ggplot(data = trainData) +
-  geom_mosaic(aes(x = product(RESOURCE, MGR_ID), fill = ACTION))
+# EDA
+# trainData <- trainData %>%
+#   mutate(ACTION = as.factor(ACTION)) %>%
+#   mutate(RESOURCE = as.factor(RESOURCE)) %>%
+#   mutate(MGR_ID = as.factor(MGR_ID)) %>%
+#   mutate(ROLE_ROLLUP_1 = as.factor(ROLE_ROLLUP_1)) %>%
+#   mutate(ROLE_ROLLUP_2 = as.factor(ROLE_ROLLUP_2)) %>%
+#   mutate(ROLE_DEPTNAME = as.factor(ROLE_DEPTNAME)) %>%
+#   mutate(ROLE_TITLE = as.factor(ROLE_TITLE)) %>%
+#   mutate(ROLE_FAMILY_DESC = as.factor(ROLE_FAMILY_DESC)) %>%
+#   mutate(ROLE_FAMILY = as.factor(ROLE_FAMILY)) %>%
+#   mutate(ROLE_CODE = as.factor(ROLE_CODE))
+# 
+# # step_mutate_at(all_numeric_predictors(), fn = factor)
+# 
+# 
+# ggplot(data = trainData) +
+#   geom_mosaic(aes(x = product(RESOURCE, MGR_ID), fill = ACTION))
 
 ############################################################################
 
@@ -72,68 +72,100 @@ amazon_predictions <- predict(logreg_wf,
 
 ############################################################################
 
-Penalized Logistic Regression
+# Penalized Logistic Regression
+# 
+# penalized_logistic <- logistic_reg(mixture=tune(), penalty=tune()) %>%
+#   set_engine("glmnet")
+# 
+# amazon_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(penalized_logistic)
+# tuning_grid <- grid_regular(penalty(),
+#                             mixture(),
+#                             levels = 5)
+# 
+# folds <- vfold_cv(trainData, v = 5, repeats=1)
+# 
+# CV_results <- amazon_workflow %>%
+#     tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# 
+# bestTune <- CV_results %>%
+#   select_best(metric = "roc_auc")
+# 
+# final_wf <- amazon_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=trainData)
+# 
+# amazon_predictions <- final_wf %>%
+#   predict(new_data = testData, type= 'prob')
 
-penalized_logistic <- logistic_reg(mixture=tune(), penalty=tune()) %>%
-  set_engine("glmnet")
 
-amazon_workflow <- workflow() %>%
+############################################################################
+
+# # Binary Random Forests
+# RF_Binary_mod <- rand_forest(mtry = tune(),
+#                       min_n=tune(),
+#                       trees=500) %>%
+#   set_engine("ranger") %>%
+#   set_mode("classification")
+# 
+# RF_Binary_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(RF_Binary_mod)
+# tuning_grid <- grid_regular(mtry(range = c(1, 9)),
+#                             min_n(),
+#                             levels = 5)
+# 
+# folds <- vfold_cv(trainData, v = 5, repeats=1)
+# 
+# CV_results <- RF_Binary_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc, f_meas, sens, recall, precision, accuracy))
+# 
+# bestTune <- CV_results %>%
+#   select_best(metric = "roc_auc")
+# 
+# final_wf <- RF_Binary_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=trainData)
+# 
+# amazon_predictions <- final_wf %>%
+#   predict(new_data = testData, type= 'prob')
+
+############################################################################
+
+# K-Nearest Neighbors
+
+knn_model <- nearest_neighbor(neighbors=tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kknn")
+
+knn_wf <- workflow() %>%
   add_recipe(my_recipe) %>%
-  add_model(penalized_logistic)
-tuning_grid <- grid_regular(penalty(),
-                            mixture(),
-                            levels = 5)
+  add_model(knn_model)
+
+tuning_grid <- grid_regular(neighbors(),
+                levels = 5)
 
 folds <- vfold_cv(trainData, v = 5, repeats=1)
 
-CV_results <- amazon_workflow %>%
-    tune_grid(resamples=folds,
+CV_results <- knn_wf %>%
+  tune_grid(resamples=folds,
             grid=tuning_grid,
             metrics=metric_set(roc_auc))
 
 bestTune <- CV_results %>%
   select_best(metric = "roc_auc")
 
-final_wf <- amazon_workflow %>%
+final_wf <- knn_wf %>%
   finalize_workflow(bestTune) %>%
   fit(data=trainData)
 
 amazon_predictions <- final_wf %>%
-  predict(new_data = testData, type= 'prob')
-
-
-############################################################################
-
-# Binary Random Forests
-RF_Binary_mod <- rand_forest(mtry = tune(),
-                      min_n=tune(),
-                      trees=500) %>%
-  set_engine("ranger") %>%
-  set_mode("classification")
-
-RF_Binary_workflow <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(RF_Binary_mod)
-tuning_grid <- grid_regular(mtry(range = c(1, 9)),
-                            min_n(),
-                            levels = 5)
-
-folds <- vfold_cv(trainData, v = 5, repeats=1)
-
-CV_results <- RF_Binary_workflow %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc, f_meas, sens, recall, precision, accuracy))
-
-bestTune <- CV_results %>%
-  select_best(metric = "roc_auc")
-
-final_wf <- RF_Binary_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=trainData)
-
-amazon_predictions <- final_wf %>%
-  predict(new_data = testData, type= 'prob')
+  predict(new_data = testData, type = 'prob')
 
 ############################################################################
 
@@ -143,7 +175,7 @@ submission <- amazon_predictions %>%
     rename(ACTION = .pred_1) %>%
     rename(ID = id)
   
-vroom_write(submission, file = "./AmazonPreds.csv", delim = ",")
+vroom_write(submission, file = "./AmazonPreds_Batch.csv", delim = ",")
 
 # FileZIlla: sftp://stat-u01.byu.edu
 # Run a batch on the Stat Servers,R CMD BATCH --no-save --no-restore AmazonAcess.R &
